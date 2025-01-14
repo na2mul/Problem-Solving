f324c24afc4ad6d94f2e33fb39366102a862e22b
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-01-14 13:37:20 +0600

    Missing_Number

---
+++ CSES Problem Set/Missing_Number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main()
+{
+   long long n;
+   long long sum=0;
+   cin >> n;
+   for(int i = 0; i<n-1; i++)
+   {
+    int x;
+    cin >> x;
+    sum+=x;
+   }
+   cout << (((n+1)*n)/2)-sum;
+    
+}
