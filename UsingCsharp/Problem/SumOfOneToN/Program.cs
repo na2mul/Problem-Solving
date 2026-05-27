607cad4c184c9693453f7843631a5fcfb4ea6938
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-28 02:04:28 +0600

    sum of N value using recursion

---
+++ UsingCsharp/Problem/SumOfOneToN/Program.cs
@@ -0,0 +1,4 @@
+﻿using SumOfNValueUsingRecursion;
+
+var sum = new Sum();
+Console.WriteLine(sum.AddValueOneToN(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
---
+++ UsingCsharp/Problem/SumOfOneToN/Sum.cs
@@ -0,0 +1,12 @@
+﻿namespace SumOfNValueUsingRecursion
+{
+    public class Sum
+    {
+        public int AddValueOneToN(List<int> list, int i = 0)
+        {
+            if(i == list.Count)
+                return 0;
+            return list[i] + AddValueOneToN(list, i+1);
+        }
+    }
+}
