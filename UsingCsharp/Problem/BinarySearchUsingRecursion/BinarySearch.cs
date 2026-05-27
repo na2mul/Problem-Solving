b2c54ca2f4b7c214f4d4c30cf18df5588e3ea5c5
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-28 01:21:43 +0600

    Binary Search using recursion

---
+++ UsingCsharp/Problem/BinarySearchUsingRecursion/BinarySearch.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearchUsingRecursion
+{
+    public class BinarySearch
+    {
+        public int Sort(List<int> list, int target, int l, int r)
+        {
+            int mid = l + ((r - l) / 2);
+
+            if (l > r)
+                return -1;
+
+            if (list[mid] == target)
+                return mid;
+
+            if (list[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+            return Sort(list, target, l, r);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/BinarySearchUsingRecursion/Program.cs
@@ -0,0 +1,11 @@
+﻿using BinarySearchUsingRecursion;
+
+var binarySearch = new BinarySearch();
+
+var list = new List<int>() { 1, 3, 5, 7, 9 };
+
+for(int i = 0; i <= 10 ; i++)
+{
+    int val = binarySearch.Sort(list, i, 0, list.Count - 1);
+    Console.WriteLine( val > -1 ? $"{i} Found at index: {val}" :  $"{i} No Found: {val}");
+}
