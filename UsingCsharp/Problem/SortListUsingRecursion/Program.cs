fbb6624aea9f45a52238e1c52b1d6e61e034c74b
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-28 00:45:58 +0600

    Sort  list number using recursion

---
+++ UsingCsharp/Problem/SortListUsingRecursion/Program.cs
@@ -0,0 +1,10 @@
+﻿using SortListUsingRecursion;
+
+var sort = new SortList();
+var list = new List<int>() { 2, 1, 5, 3, 4 };
+sort.Sort(list);
+
+foreach(var item in list)
+{
+    Console.Write(item + " ");
+}
---
+++ UsingCsharp/Problem/SortListUsingRecursion/SortList.cs
@@ -0,0 +1,27 @@
+﻿namespace SortListUsingRecursion
+{
+    public class SortList
+    {
+        public void Sort(List<int> list)
+        {
+            if (list.Count == 1)
+                return;
+            int temp = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            Sort(list);
+            Insert(list, temp);
+        }
+        public void Insert(List<int> list, int temp)
+        {
+            if (list.Count == 0 || list[list.Count - 1] <= temp)
+            {
+                list.Add(temp);
+                return;
+            }
+            int value = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            Insert(list, temp);
+            list.Add(value);
+        }
+    }
+}
