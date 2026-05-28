ad9ea480063db2e3ba4bcece58b9c43197d3252d
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-28 23:41:48 +0600

    delete stack middle value using recursion

---
+++ UsingCsharp/Problem/DeleteStackMiddleValueUsingRecursion/DeleteStackMiddleValue.cs
@@ -0,0 +1,18 @@
+﻿namespace DeleteStackMiddleValueUsingRecursion
+{
+    public class DeleteStackMiddleValue
+    {
+       public void DeleteMiddleValue(Stack<int> v, int k)
+        {
+            if(k == 1)
+            {
+                v.Pop();
+                return;
+            }
+            int temp = v.First();
+            v.Pop();
+            DeleteMiddleValue(v, k - 1);
+            v.Push(temp);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/DeleteStackMiddleValueUsingRecursion/Program.cs
@@ -0,0 +1,14 @@
+﻿using DeleteStackMiddleValueUsingRecursion;
+
+var deleteStackMiddleValue = new DeleteStackMiddleValue();
+var stack = new Stack<int>();
+for (int i = 1; i <= 9; i++)
+{
+    stack.Push(i);
+}
+deleteStackMiddleValue.DeleteMiddleValue(stack, stack.Count / 2 + 1);
+
+foreach(var item in stack)
+{
+    Console.Write( item + " ");
+}
