aac7ed70eaaf326180b73e525cf6f5422713da53
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-29 16:01:00 +0600

    Delete First Occurrence Value from list

---
+++ UsingCsharp/Problem/DeleteFirstOccurrenceValueFromListUsingRecursion/DeleteFirstOccurrenceValueFromList.cs
@@ -0,0 +1,38 @@
+﻿namespace DeleteFirstOccurrenceValueFromListUsingRecursion
+{
+    public class DeleteFirstOccurrenceValueFromList
+    {
+        public int DeleteFirstOccurrenceValue(List<int> list, int value, int index = 0)
+        {
+            return Find(list, value, index);
+        }
+
+        private int Find(List<int> list, int value, int index)
+        {
+            if (index == list.Count)
+            {
+                return -1;
+            }
+
+            if (list[index] == value)
+            {
+                Replace(list, index);
+                RemoveLastIndex(list);
+                return 1;
+
+            }
+            return Find(list, value, index + 1);
+        }
+        private void RemoveLastIndex(List<int> list)
+        {
+            list.RemoveAt(list.Count - 1);
+        }
+        private void Replace(List<int> list, int index)
+        {
+            if (index == list.Count - 1)
+                return;
+            list[index] = list[index + 1];
+            Replace(list, index + 1);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/DeleteFirstOccurrenceValueFromListUsingRecursion/Program.cs
@@ -0,0 +1,16 @@
+﻿using DeleteFirstOccurrenceValueFromListUsingRecursion;
+
+var deleteFirstOccurrenceValueFromList = new DeleteFirstOccurrenceValueFromList();
+var list = new List<int>() { 1, 4, 3, 4, 5, 6 };
+
+Console.Write("Before     ");
+foreach (var item in list)
+    Console.Write(item + " ");
+Console.WriteLine();
+
+int value = deleteFirstOccurrenceValueFromList.DeleteFirstOccurrenceValue(list, 4);
+
+Console.WriteLine(value > -1 ? "Value Found" : "Value not Found");
+Console.Write("After      ");
+foreach (var item in list)
+    Console.Write(item + " ");
