f34279e8c766e7a21b9bf90c61fb40963c800eb5
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-01 01:39:55 +0600

    find Kth symbol in grammar using recursion

---
+++ UsingCsharp/Problem/FindKthSymbolInGrammarUsingRecursion/FindKthSymbolInGrammar.cs
@@ -0,0 +1,17 @@
+﻿namespace FindKthSymbolInGrammarUsingRecursion
+{
+    public class FindKthSymbolInGrammar
+    {
+        public bool FindKthGrammar(int n, int k)
+        {
+            if(n == 1 &&  k == 1) return false;
+
+            int mid = (int)Math.Pow(2 ,n - 1)/2;
+
+            if(k <= mid)
+                return FindKthGrammar(n-1, k);
+            else
+                return !FindKthGrammar(n, k-mid);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/FindKthSymbolInGrammarUsingRecursion/Program.cs
@@ -0,0 +1,13 @@
+﻿using FindKthSymbolInGrammarUsingRecursion;
+
+var findKthGrammar = new FindKthSymbolInGrammar();
+
+for (int n = 1; n<=4; n++)
+{
+    int m = (int)Math.Pow(2, n - 1);
+	for (int k = 1; k <= m; k++)
+	{
+        Console.Write(findKthGrammar.FindKthGrammar(n, k) + " ");
+    }
+    Console.WriteLine();
+}
