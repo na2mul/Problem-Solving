3c0fd8be1cc1414a07ffcf470da7fe8a9470fe95
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-05-31 13:52:46 +0600

    Reverse stack using recursion

---
+++ UsingCsharp/Problem/ReverseStackUsingRecursion/Program.cs
@@ -0,0 +1,17 @@
+﻿using ReverseStackUsingRecursion;
+
+var reverseStack = new ReverseStack();
+
+var stack = new Stack<int>();
+
+for (int i = 1; i <= 9; i++)
+{
+    stack.Push(i);
+}
+
+reverseStack.Reverse(stack);
+
+foreach(var item in stack)
+{
+    Console.WriteLine(item);
+}
---
+++ UsingCsharp/Problem/ReverseStackUsingRecursion/ReverseStack.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ReverseStackUsingRecursion
+{
+    public class ReverseStack
+    {
+        public void Reverse(Stack<int> stack)
+        {
+            if (stack.Count == 1)
+                return;
+            int temp = stack.First();
+            stack.Pop();
+            Reverse(stack);
+            Insert(stack, temp);
+        }
+
+        private void Insert(Stack<int> stack, int temp)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(temp);
+                return;
+            }
+            int value = stack.First();
+            stack.Pop();
+            Insert(stack, temp);
+            stack.Push(value);
+        }
+    }
+}
