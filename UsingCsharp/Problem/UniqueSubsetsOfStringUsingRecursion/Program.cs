c5015a4f593cfd2f3ffbc3057fb4843449d6ee9c
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-08 11:50:44 +0600

    unique subsets of string using recursion

---
+++ UsingCsharp/Problem/UniqueSubsetsOfStringUsingRecursion/Program.cs
@@ -0,0 +1,10 @@
+﻿using UniqueSubsetsOfStringUsingRecursion;
+
+var uniqueSubsetsOfString = new UniqueSubsetsOfString();
+
+Console.Write("\" \"");
+var map = new HashSet<string>();
+uniqueSubsetsOfString.UniqueSubsets("", "aab",map);
+
+foreach(var subset in map)
+    Console.Write(subset + ", ");
---
+++ UsingCsharp/Problem/UniqueSubsetsOfStringUsingRecursion/UniqueSubsetsOfString.cs
@@ -0,0 +1,24 @@
+﻿namespace UniqueSubsetsOfStringUsingRecursion
+{
+    public class UniqueSubsetsOfString
+    {
+        public HashSet<string> UniqueSubsets(string output, string input, HashSet<string> map)
+        {
+            if (input.Length == 0)
+            {
+                map.Add(output);
+                return map;
+            }
+            string output1 = output;
+            string output2 = output;
+
+            output2 = output2.Insert(0, input[input.Length - 1].ToString());
+            input = input.Remove(input.Length - 1, 1);
+
+            UniqueSubsets(output1, input, map);
+            UniqueSubsets(output2, input, map);
+
+            return map;
+        }
+    }
+}
