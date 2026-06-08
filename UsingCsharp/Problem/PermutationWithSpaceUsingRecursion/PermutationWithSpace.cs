1358e03bed36b78d99c04b8d3c160632bedec0e2
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-08 13:45:14 +0600

    Permutation with space of string using Recursion

---
+++ UsingCsharp/Problem/PermutationWithSpaceUsingRecursion/PermutationWithSpace.cs
@@ -0,0 +1,25 @@
+﻿namespace PermutationWithSpaceUsingRecursion
+{
+    public class PermutationWithSpace
+    {
+        public void Permuation(string output, string input)
+        {
+            if(input.Length == 0)
+            {
+                Console.Write(output + ", ");
+                return;
+            }
+
+            string output1 = output;
+            string output2 = output;
+            output1 = output1.Insert(output1.Length, " ");
+            output1 = output1.Insert(output1.Length, input[0].ToString());
+
+            output2 = output2.Insert(output2.Length, input[0].ToString());
+            input = input.Remove(0, 1);
+
+            Permuation(output1, input);
+            Permuation(output2, input);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/PermutationWithSpaceUsingRecursion/Program.cs
@@ -0,0 +1,6 @@
+﻿using PermutationWithSpaceUsingRecursion;
+
+var permutationWithSpace = new PermutationWithSpace();
+string str = "abc";
+string output = str[0].ToString(), input = str.Remove(0, 1);
+permutationWithSpace.Permuation(output, input);
