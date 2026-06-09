a1d9a4d038336aecc08f605916164b26e25bb639
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-09 16:42:10 +0600

    maximum sum of subarray of kth slide window

---
+++ UsingCsharp/Problem/MaxSumOfSubArrayOfKthSize/MaxSumOfSubArray.cs
@@ -0,0 +1,27 @@
+﻿namespace MaxSumOfSubArrayOfKthSize
+{
+    public class MaxSumOfSubArray
+    {
+        public int MaxSum(int[] array, int k)
+        {
+            int max = int.MinValue;
+            int sum = 0;
+            int i = 0, j = 0;
+
+            while (j < array.Length)
+            {
+                if(j < k)
+                {
+                    sum += array[j++];
+                    max = int.Max(max, sum);
+                }
+                else
+                {
+                    sum = sum - array[i++] + array[j++];
+                    max = int.Max(max, sum);
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ UsingCsharp/Problem/MaxSumOfSubArrayOfKthSize/Program.cs
@@ -0,0 +1,6 @@
+﻿using MaxSumOfSubArrayOfKthSize;
+
+var maxSumOfSubArray = new MaxSumOfSubArray();
+var max = maxSumOfSubArray.MaxSum(new int[] { 22,9,19,50,7,20}, 3);
+
+Console.WriteLine(max);
