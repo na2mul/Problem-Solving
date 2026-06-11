a4e9bdb5705f1af7ca01d59d0a237bdc6995d7e7
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-11 21:51:57 +0600

    Generate all balanced Parentheses

---
+++ UsingCsharp/Problem/GenerateAllBalancedParenthesesUsingRecursion/GenerateAllBalancedParentheses.cs
@@ -0,0 +1,28 @@
+﻿namespace GenerateAllBalancedParenthesesUsingRecursion
+{
+    public class GenerateAllBalancedParentheses
+    {
+        public void AllBalancedParentheses(int openBracket, int closeBracket, string output)
+        {
+            if(openBracket == 0 && closeBracket == 0)
+            {
+                Console.Write(output + ", ");
+                return;
+            }
+
+            if(openBracket != 0)
+            {
+                string output1 = output;
+                output1 = output1.Insert(output1.Length, "(");
+                AllBalancedParentheses(openBracket - 1, closeBracket, output1);
+            }
+
+            if(openBracket < closeBracket)
+            {
+                string output2 = output;
+                output2 = output2.Insert(output2.Length, ")");
+                AllBalancedParentheses(openBracket, closeBracket - 1, output2);
+            }
+        }
+    }
+}
---
+++ UsingCsharp/Problem/GenerateAllBalancedParenthesesUsingRecursion/Program.cs
@@ -0,0 +1,4 @@
+﻿using GenerateAllBalancedParenthesesUsingRecursion;
+
+var generateAllBalancedParentheses = new GenerateAllBalancedParentheses();
+generateAllBalancedParentheses.AllBalancedParentheses(4,4, "");
