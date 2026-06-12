d75a060c6860585498697d4e4c38517a3fc779d8
Nazmul Hasan <hasanmdnazmul78@gmail.com>
2026-06-12 19:51:14 +0600

    Josephus problem find survivor number using recursion

---
+++ UsingCsharp/Problem/Recursion/JosephusProblemUsingRecursion/JosephusProblem.cs
@@ -0,0 +1,15 @@
+﻿namespace JosephusProblemUsingRecursion
+{
+    public class JosephusProblem
+    {
+        public int FindSurvivorNumber(List<int> input, int k, int index = 0)
+        {
+            if (input.Count == 1)
+                return input[0];
+
+            index = (index + k - 1) % input.Count;
+            input.RemoveAt(index);
+            return FindSurvivorNumber(input, k, index);
+        }
+    }
+}
---
+++ UsingCsharp/Problem/Recursion/JosephusProblemUsingRecursion/Program.cs
@@ -0,0 +1,14 @@
+﻿using JosephusProblemUsingRecursion;
+
+var josephusProblem = new JosephusProblem();
+
+var list = new List<int>();
+
+for(int i = 1; i<= 40; i++)
+{
+    list.Add(i);
+}
+
+int number = josephusProblem.FindSurvivorNumber(list, 7);
+
+Console.WriteLine(number);
